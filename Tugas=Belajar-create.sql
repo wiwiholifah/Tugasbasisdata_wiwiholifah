f608d40de2990a7bf3b0b04d5693a2713804ed26
wiwiholifah <110832270+wiwiholifah@users.noreply.github.com>
2022-08-15 12:29:23 +0700

    Add files via upload

---
+++ Tugas=Belajar-create.sql
@@ -0,0 +1,77 @@
+UPDATE siswa SET
+tempatlahir ="INDRAMAYU"
+WHERE
+nis ="12002406";
+
+SELECT * FROM siswa
+WHERE
+ nis ="12002406";
+
+ SELECT siswa SET
+ tanggallahir = "2005-05-05",
+ alamat = "CIGADUNG"
+ WHERE
+ nis = "12002406";
+
+ SELECT * FROM siswa
+ WHERE
+  jk = "L";
+
+ SELECT * FROM siswa
+ WHERE 
+  jk = "L" AND tempatlahir = "BANDUNG"
+
+SELECT * FROM siswa
+WHERE
+ jk = "P" OR tempatlahir = "BANDUNG";
+
+ SELECT * FROM siswa
+WHERE
+ tanggallahir BETWEEN "2004-10-01" AND "2004-12-30";
+
+ SELECT * FROM siswa
+WHERE
+ nilai BETWEEN "80" AND "90";
+
+ SELECT * FROM siswa
+WHERE
+ nama like "%O%";
+
+ SELECT * FROM siswa
+WHERE
+ nama nilai "%U%";
+
+ SELECT * FROM siswa
+WHERE
+ nama nilai "%A%";
+
+ SELECT * FROM siswa
+WHERE
+ nilai BETWEEN "80" AND "90"
+ ORDER BY nilai ASC;
+
+ SELECT * FROM siswa
+ WHERE
+ nilai BETWEEN "80" AND "90"
+ ORDER BY nilai DESC;
+
+ DELETE FROM siswa 
+ WHERE
+ nis = "12002406";
+
+ SELECT tempatlahir
+ FROM siswa
+ GROUP BY tempatlahir;
+
+ SELECT tempatlahir
+FROM siswa
+GROUP BY tempatlahir;
+
+SELECT nama,kelas,nilai
+FROM siswa;
+
+WHERE tempatlahir != "SUBANG"
+
+WHERE nilai >=80 AND <=90;
+
+CREATE DATABASE 12 RPL 1;
